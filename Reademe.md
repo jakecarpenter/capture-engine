1590eb8d36f2f5fac3071181d1ed3be8a99bce94
Jake Carpenter <jake@jakecarpenter.com>
2013-05-27 15:07:30 -0800

    Initial Commit

---
+++ Reademe.md
@@ -0,0 +1,1 @@
+This project pulls down tweets and statuses from fb and twitter and uploads them to the capture app on parse.
---
+++ auth-info.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*
+*  Include auth info for the  various services, also include settings that are relevant to all three scripts.
+*
+*/
+
+$capture_domain = "trycapture.com";
+
+$capture_event =  "TestEvent02";
+
+//db stuff
+$db_user = "capture";
+$db_pass = "2Insecure";
+$db_db = "capture";
+
+
+
+?>
